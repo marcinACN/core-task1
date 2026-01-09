4b6e180b60f2f2a717954432289de9d63930374b
Marcin Perlinski <marcin.perlinski@accenture.com>
2026-01-09 10:58:53 +0100

    Spring setup

---
+++ src/main/java/com/perlinski/core_task1/CoreTask1Application.java
@@ -0,0 +1,13 @@
+package com.perlinski.core_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/perlinski/core_task1/CoreTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perlinski.core_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
